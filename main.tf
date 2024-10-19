d6dc3c2d712dfc95803d21099ea21d3c79a3e738
Vedant <93828050+vedan666@users.noreply.github.com>
2024-10-19 14:05:10 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
   count    = var.create_rg ? 1 : 0
-  name     = "rg-chonk-${count.index + 1}"
+  name     = "rg-dev-${count.index + 1}"
   location = "westus"
 }
